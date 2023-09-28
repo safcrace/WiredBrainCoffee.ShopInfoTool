d8c171809127af42410ec89b76c5023d1558d477
Sender Flores <safcrace@gmail.com>
2023-09-28 17:33:08 -0600

    Add coffee shop data provider

---
+++ WiredBrainCoffee.ShopInfoTool/Data/CoffeShopDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using WiredBrainCoffee.ShopInfoTool.Model;
+
+namespace WiredBrainCoffee.ShopInfoTool.Data;
+
+public class CoffeShopDataProvider
+{
+    public IEnumerable<CoffeeShop> LoadShops()
+    {
+        return new CoffeeShop[]
+        {
+            new CoffeeShop("frankfurt", 45),
+            new CoffeeShop("Zurich", 39),
+            new CoffeeShop("Rome", 25),
+        };
+    }
+}
