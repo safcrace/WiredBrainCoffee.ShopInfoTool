d6df8fb5d9599446bd0384abb221ceb1bae41a2e
Sender Flores <safcrace@gmail.com>
2023-09-28 18:11:16 -0600

    Implement quit command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,5 +1,15 @@
 ﻿Console.WriteLine("-------------------------------------");
 Console.WriteLine(" Wired Brain Coffee - Shop Info Tool ");
 Console.WriteLine("-------------------------------------");
+Console.WriteLine();
+Console.WriteLine("Commands:");
+Console.WriteLine("   quit   exit application");
 
-Console.ReadLine();
+while (true)
+{
+    var command = Console.ReadLine();
+    if (string.Equals("quit", command, StringComparison.OrdinalIgnoreCase))
+    {
+        break;
+    }
+}
