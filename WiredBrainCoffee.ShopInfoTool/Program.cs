88cddb5654f0c96ce2a8b5497cc8246411abf8a7
Sender Flores <safcrace@gmail.com>
2023-09-29 18:36:15 -0600

    Implement shops command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,9 +1,12 @@
-﻿Console.WriteLine("-------------------------------------");
+﻿using WiredBrainCoffee.ShopInfoTool.Data;
+
+Console.WriteLine("-------------------------------------");
 Console.WriteLine(" Wired Brain Coffee - Shop Info Tool ");
 Console.WriteLine("-------------------------------------");
 Console.WriteLine();
 Console.WriteLine("Commands:");
 Console.WriteLine("   quit   exit application");
+Console.WriteLine("   shops  show coffeeshops");
 
 while (true)
 {
@@ -11,5 +14,15 @@ while (true)
     if (string.Equals("quit", command, StringComparison.OrdinalIgnoreCase))
     {
         break;
+    }
+    else if (string.Equals("shops", command, StringComparison.OrdinalIgnoreCase))
+    {
+        var dataProvider = new CoffeShopDataProvider();
+        var coffeeShops = dataProvider.LoadShops();
+
+        foreach ( var coffeeShop in coffeeShops)
+        {
+            Console.WriteLine(coffeeShop);
+        }
     }
 }
